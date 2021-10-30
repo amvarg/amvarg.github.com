46a8227f1a587fb04f42bf9befb8325476271258
Alessandra Vargas <amvarg@ucdavis.edu>
2021-10-29 21:55:15 -0700

    Mad Libs 2.0 Update 10/29

--- des157/studio1/script.js
+++ des157/studio1/script.js
@@ -4,7 +4,14 @@
 
     var myForm = document.querySelector(`#myForm`);
     var madlib = document.querySelector("#madlib");
-
+    var incomplete = document.querySelector("#incomplete")
+    var posterOverlay = document.querySelector("#posterOverlay")
+    var artistName = document.querySelector("#artistName")
+    var performing = document.querySelector("#performing")
+    var fruitEmo = document.querySelector("#fruitEmo")
+    var home = document.querySelector("#home")
+    var venueDate = document.querySelector("#venueDate")
+    let selImg = document.querySelector('#selImg');
    
 
     // selDrop.addEventListener('submit', function(event){
@@ -14,6 +21,8 @@
    
     myForm.addEventListener('submit', function(event){
         event.preventDefault();
+
+        //user input variables
         var adj1 = document.querySelector('#adj1').value;
         var noun1 = document.querySelector('#noun1').value;
         var fruit = document.querySelector('#fruit').value;
@@ -27,15 +36,67 @@
         var num2 = document.querySelector('#num2').value;
         var selDrop = document.querySelector('select').value;
         
+        //message 2 user
         var myText;
 
-      
-        if(adj1 && noun1 && fruit && emo && verb1 && noun2 && adj2 && room && hometown && num1 && num2){
+        // 
+        if(adj1 && noun1 && fruit && emo && verb1 && noun2 && adj2 && room && hometown && num1 && num2 && selDrop === "hardcore"){
             myText =  `LIVE in concert. The ${adj1} ${noun1}, with special guests ${fruit} ${emo}. Performing their hit single ${verb1} on the ${noun2}. The ${adj2} ${room}. October 25, 1970. ${hometown}, California. ${num1}:${num2} PM, Come feeling ${selDrop}.`;
-    
+            posterOverlay.style.display = "block";
+            myForm.innerHTML="";//clears everything out
+            artistName.innerHTML = ` The ${adj1} ${noun1}`;
+            selImg.setAttribute('src', `images/hardcore.png`);
+            performing.innerHTML = `Performing their hit single ${verb1} on the ${noun2}.`
+            fruitEmo.innerHTML =`${fruit} ${emo}`;
+            home.innerHTML =`${hometown}, California.`;
+            venueDate.innerHTML = `The${adj2} ${room}, ${num1}:${num2} PM.`
+            console.log("hope u like ur poster :). this picture is one i took seeing the strokes at the forum.")
+            
         }
+        else if (adj1 && noun1 && fruit && emo && verb1 && noun2 && adj2 && room && hometown && num1 && num2 && selDrop === "relaxed"){
+            myText =  `LIVE in concert. The ${adj1} ${noun1}, with special guests ${fruit} ${emo}. Performing their hit single ${verb1} on the ${noun2}. The ${adj2} ${room}. October 25, 1970. ${hometown}, California. ${num1}:${num2} PM, Come feeling ${selDrop}.`;
+            posterOverlay.style.display = "block";
+            myForm.innerHTML="";//clears everything out
+            artistName.innerHTML = ` The ${adj1} ${noun1}`;
+            selImg.setAttribute('src', `images/relaxed.png`);
+            performing.innerHTML = `Performing their hit single ${verb1} on the ${noun2}.`
+            fruitEmo.innerHTML =`${fruit} ${emo}`;
+            home.innerHTML =`${hometown}, California.`;
+            venueDate.innerHTML = `The${adj2} ${room}, ${num1}:${num2} PM.`
+            console.log("hope u like ur poster :). this picture is one i took at an album release party.")
+
+        }
+
+        else if (adj1 && noun1 && fruit && emo && verb1 && noun2 && adj2 && room && hometown && num1 && num2 && selDrop === "danceable"){
+            myText =  `LIVE in concert. The ${adj1} ${noun1}, with special guests ${fruit} ${emo}. Performing their hit single ${verb1} on the ${noun2}. The ${adj2} ${room}. October 25, 1970. ${hometown}, California. ${num1}:${num2} PM, Come feeling ${selDrop}.`;
+            posterOverlay.style.display = "block";
+            myForm.innerHTML="";//clears everything out
+            artistName.innerHTML = ` The ${adj1} ${noun1}`;
+            selImg.setAttribute('src', `images/danceable.png`);
+            performing.innerHTML = `Performing their hit single ${verb1} on the ${noun2}.`
+            fruitEmo.innerHTML =`${fruit} ${emo}`;
+            home.innerHTML =`${hometown}, California.`;
+            venueDate.innerHTML = `The${adj2} ${room}, ${num1}:${num2} PM.`
+            console.log("hope u like ur poster :). this picture is one i took on my 21st bday.")
+        }
+
+        else if (adj1 && noun1 && fruit && emo && verb1 && noun2 && adj2 && room && hometown && num1 && num2 && selDrop === "mind-blowing"){
+            myText =  `LIVE in concert. The ${adj1} ${noun1}, with special guests ${fruit} ${emo}. Performing their hit single ${verb1} on the ${noun2}. The ${adj2} ${room}. October 25, 1970. ${hometown}, California. ${num1}:${num2} PM, Come feeling ${selDrop}.`;
+            posterOverlay.style.display = "block";
+            myForm.innerHTML="";//clears everything out
+            artistName.innerHTML = ` The ${adj1} ${noun1}`;
+            selImg.setAttribute('src', `images/mind-blowing.png`);
+            performing.innerHTML = `Performing their hit single ${verb1} on the ${noun2}.`
+            fruitEmo.innerHTML =`${fruit} ${emo}`;
+            home.innerHTML =`${hometown}, California.`;
+            venueDate.innerHTML = `The${adj2} ${room}, ${num1}:${num2} PM.`
+            console.log("hope u like ur poster :). this picture is one i took seeing the band hippo campus.")
+        }
+
         else {
-            myText =     'Please complete the form so I can make your Mad Lib!'
+            myText =''
+            incomplete.innerHTML = "Please complete the form so <br> I can make your Mad Lib!";
+            
         }
 
         madlib.innerHTML = myText;
